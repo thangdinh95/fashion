c7cd6cf84f3ca49f672ac0e7c6622d35ab361c57
thangdinh95 <tdxxi95@gmail.com>
2018-01-22 23:54:20 -1200

    dfgh

---
+++ project_22.1/js/slide.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+  $('.slide-discountproduct').slick({
+  prevArrow: '<div class="slick-next"><span class="fa fa-angle-left"></span><span class="sr-only">Prev</span></div>',
+
+	nextArrow: '<div class="slick-prev"><span class="fa fa-angle-right"></span><span class="sr-only">Next</span></div>'
+});
+
+  });
+	
